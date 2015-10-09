80fa74630a81d7cff41779b2eb93b7f6c1c0fb77
megan <misscloudfall@gmail.com>
2015-10-09 13:11:45 -0700

    The original commit for hello world project

---
+++ Assignment_1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World\n";
+   cout << "Family name: Wu\n";
+   cout << "Student ID: 20081228\n" << endl;
+ 
+   cout << "Syllabus 1: There are such things as bad questions.\n"
+      << "Syllabus 2: You don't have to post in discussion, but"
+      << "you should.\n" << endl;
+
+   cout << "Lab Requirements 1: A working program is better than an"
+      << "on-time program.\n"
+      << "Lab Requirements 2: Only use what we have learned in class.\n"
+      << "Lab Requirements 3: A late program will cost you points,"
+      << "but an early program \n"
+      << "may cost you too.\n" << endl;
+
+   return 0;
+}
+
+
+
+/* ----------- paste of run ----------
+
+Hello World
+Family name: Wu
+Student ID: 20081228
+
+Syllabus 1: There are such things as bad questions.
+Syllabus 2: You don't have to post in discussion, butyou should.
+
+Lab Requirements 1: A working program is better than anon-time program.
+Lab Requirements 2: Only use what we have learned in class.
+Lab Requirements 3: A late program will cost you points,but an early program
+may cost you too.
+
+Press any key to continue . . .
+
+-------------------------------------*/
